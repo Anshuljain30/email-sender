539277af687e67c224596ae5a3bf7e16060d44e4
Anshul Jain <anshul_jain@live.com>
2024-08-07 18:33:14 +0530

    minor fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { GoogleAuthService } from './google-auth/google-auth.service';
 import { GmailService } from './gmail/gmail.service';
 import { EmailController } from './email/email.controller';
 import { ExcelService } from './excel/excel.service';
-import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, EmailController, AuthController],
+  controllers: [AppController, EmailController],
   providers: [AppService, GoogleAuthService, GmailService, ExcelService],
 })
 export class AppModule {}
