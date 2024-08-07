6b5202e1a2ed48206735f24e915c2f1c0d33971c
Anshul Jain <anshul_jain@live.com>
2024-08-07 13:27:39 +0530

    added gmail service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleAuthService } from './google-auth/google-auth.service';
+import { GmailService } from './gmail/gmail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, GoogleAuthService],
+  providers: [AppService, GoogleAuthService, GmailService],
 })
 export class AppModule {}
