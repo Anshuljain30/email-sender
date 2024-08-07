7863f3083c959f9d2c82fac38b7b8eb8c2805f93
Anshul Jain <anshul_jain@live.com>
2024-08-07 13:32:31 +0530

    updated excel service

--- src/excel/excel.service.ts
+++ src/excel/excel.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import * as ExcelJS from 'exceljs';
 
 @Injectable()
-export class ExcelService {}
+export class ExcelService {
+  async readExcel(
+    filePath: string,
+  ): Promise<{ name: string; email: string }[]> {
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+    const worksheet = workbook.worksheets[0];
+    const data = [];
+
+    worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+      if (rowNumber === 1) return; // Skip header row
+
+      const name = row.getCell(1).value;
+      const email = row.getCell(2).value;
+
+      data.push({ name, email });
+    });
+
+    return data;
+  }
+}
