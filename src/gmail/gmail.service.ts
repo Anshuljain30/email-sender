f8c941e7b08d7fb27867c06704c0f2a20679225b
Anshul Jain <anshul_jain@live.com>
2024-08-07 13:28:33 +0530

    updated gmail service

--- src/gmail/gmail.service.ts
+++ src/gmail/gmail.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
+import { GoogleAuthService } from '../google-auth/google-auth.service';
 
 @Injectable()
-export class GmailService {}
+export class GmailService {
+  private gmail;
+
+  constructor(private readonly googleAuthService: GoogleAuthService) {
+    this.gmail = google.gmail({
+      version: 'v1',
+      auth: this.googleAuthService.getOAuth2Client(),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    text: string,
+    attachmentPath: string,
+  ) {
+    const { createReadStream } = require('fs');
+    const { google } = require('googleapis');
+    const mime = require('mime-types');
+    const base64url = require('base64-url');
+
+    const boundary = '---';
+    const messageParts = [
+      `From: your-email@gmail.com`,
+      `To: ${to}`,
+      `Subject: ${subject}`,
+      `Content-Type: multipart/mixed; boundary="${boundary}"`,
+      '',
+      `--${boundary}`,
+      `Content-Type: text/plain; charset="UTF-8"`,
+      '',
+      text,
+      '',
+      `--${boundary}`,
+      `Content-Type: ${mime.lookup(attachmentPath)}; name="${attachmentPath
+        .split('/')
+        .pop()}"`,
+      `Content-Disposition: attachment; filename="${attachmentPath
+        .split('/')
+        .pop()}"`,
+      `Content-Transfer-Encoding: base64`,
+      '',
+      base64url.encode(createReadStream(attachmentPath).toString('base64')),
+      `--${boundary}--`,
+    ];
+
+    const rawMessage = messageParts.join('\n');
+
+    const res = await this.gmail.users.messages.send({
+      userId: 'me',
+      requestBody: {
+        raw: base64url.encode(rawMessage),
+      },
+    });
+
+    return res.data;
+  }
+}
