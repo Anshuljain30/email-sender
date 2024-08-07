dfd8850cdb3464365c649c2f9f7e330f35337bd7
Anshul Jain <anshul_jain@live.com>
2024-08-07 13:31:16 +0530

    added excel service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { GoogleAuthService } from './google-auth/google-auth.service';
 import { GmailService } from './gmail/gmail.service';
 import { EmailController } from './email/email.controller';
+import { ExcelService } from './excel/excel.service';
 
 @Module({
   imports: [],
   controllers: [AppController, EmailController],
-  providers: [AppService, GoogleAuthService, GmailService],
+  providers: [AppService, GoogleAuthService, GmailService, ExcelService],
 })
 export class AppModule {}
