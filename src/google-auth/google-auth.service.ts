e2ef068441422ad5419be80ec6ac6dbbbd662536
Anshul Jain <anshul_jain@live.com>
2024-08-07 13:27:26 +0530

    added auth service

--- src/google-auth/google-auth.service.ts
+++ src/google-auth/google-auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
 
 @Injectable()
-export class GoogleAuthService {}
+export class GoogleAuthService {
+  private oauth2Client;
+
+  constructor() {
+    const { client_id, client_secret, redirect_uris } =
+      require('./credentials.json').installed;
+
+    this.oauth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0],
+    );
+  }
+
+  async getAuthUrl() {
+    const scopes = ['https://www.googleapis.com/auth/gmail.send'];
+    const url = this.oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes,
+    });
+    return url;
+  }
+
+  async getToken(code: string) {
+    const { tokens } = await this.oauth2Client.getToken(code);
+    this.oauth2Client.setCredentials(tokens);
+    return tokens;
+  }
+
+  getOAuth2Client() {
+    return this.oauth2Client;
+  }
+}
